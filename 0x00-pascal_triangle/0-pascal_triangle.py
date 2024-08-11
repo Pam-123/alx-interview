e84836a0b449f3fc85dba74e61da08c948269352
Pam-123 <pamschawira@gmail.com>
2024-08-11 09:45:48 -0700

    --

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,4 @@
-﻿#!/usr/bin/python3
-
-"""
-0. Pascal's Triangle
-"""
-
-def pascal_triangle(n):
-    """Create a function def pascal_triangle(n): that returns a list of lists
-    of integers representing the Pascal’s triangle of n
-    """
+﻿def pascal_triangle(n):
     res = []
     if n > 0:
         for i in range(n):
