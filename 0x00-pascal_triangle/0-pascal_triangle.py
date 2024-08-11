acdd051c677a254ab75c6fb9b91c79cda961da41
Pam-123 <pamschawira@gmail.com>
2024-08-11 10:48:13 -0700

    Why

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,4 @@
 ﻿#!/usr/bin/python3
-
 """
 This module calculate n pascal triangle
 """
@@ -16,12 +15,12 @@ def pascal_triangle(n):
     triangle = [[1]]  # Initialize the triangle with the first row
 
     for i in range(1, n):
-        previous_row = triangle[-1]  # Get the last row of the triangle
-        next_row = [1]  # Start the new row with 1
+        prev_row = triangle[-1]  # Get the last row of the triangle
+        new_row = [1]  # Start the new row with 1
         for j in range(1, i):
             # Compute the value by summing the two values above it
-            next_row.append(previous_row[j - 1] + previous_row[j])
-        next_row.append(1)  # End the row with 1
-        triangle.append(next_row)
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        new_row.append(1)  # End the row with 1
+        triangle.append(new_row)
 
     return triangle
