bf3e9152a259fbc54f77f590edeb734b459c30f5
Pam-123 <pamschawira@gmail.com>
2024-08-11 10:35:39 -0700

    Pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,27 +1,51 @@
 ﻿#!/usr/bin/python3
-"""Pascal Triangle Interview Challenge"""
+"""
+Pascal's Triangle Module
+
+This module provides a function to generate Pascal's Triangle up to a given number of rows.
+"""
 
 
 def pascal_triangle(n):
-    """returns a list of lists of numbers
-    representing the pascal triangle"""
+    """
+    Generates Pascal's Triangle up to the nth row.
+
+    Args:
+        n (int): The number of rows in Pascal's Triangle.
+
+    Returns:
+        list of lists: A list of lists of integers representing Pascal's Triangle.
+                       Each inner list represents a row in the triangle.
+                       Returns an empty list if n <= 0.
+    """
     if n <= 0:
         return []
+    
+    triangle = [[1]]  # Initialize the first row
+    
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        # Start the new row with a 1
+        new_row = [1]
+        # Generate the intermediate values by adding the adjacent elements in the previous row
+        for j in range(1, len(prev_row)):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        # End the new row with a 1
+        new_row.append(1)
+        triangle.append(new_row)
+    
+    return triangle
 
-    pascal_triangle = [0] * n
 
-    for i in range(n):
-        # define a row and fill first and last idx with 1
-        new_row = [0] * (i+1)
-        new_row[0] = 1
-        new_row[len(new_row) - 1] = 1
+if __name__ == "__main__":
+    def print_triangle(triangle):
+        """
+        Prints Pascal's Triangle in a formatted way.
 
-        for j in range(1, i):
-            if j > 0 and j < len(new_row):
-                a = pascal_triangle[i - 1][j]
-                b = pascal_triangle[i - 1][j - 1]
-                new_row[j] = a + b
-
-        pascal_triangle[i] = new_row
-
-    return pascal_triangle
+        Args:
+            triangle (list of lists): The Pascal's Triangle to print.
+        """
+        for row in triangle:
+            print("[{}]".format(",".join([str(x) for x in row])))
+    
+    print_triangle(pascal_triangle(5))
